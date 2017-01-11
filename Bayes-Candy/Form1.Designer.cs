ff099a94f993202877f912d6804cdbd3f1f3ab62
Darien Hoang <dhoang1@asu.edu>
2017-01-11 09:47:17 -0700

    v1.0 commit

---
+++ Bayes-Candy/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.AxisX.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.True;
+            chartArea2.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea2.AxisX.IsMarginVisible = false;
+            chartArea2.AxisX.MajorGrid.Enabled = false;
+            chartArea2.AxisX.Minimum = 0D;
+            chartArea2.AxisX.Title = "Number of observations in D";
+            chartArea2.AxisY.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.True;
+            chartArea2.AxisY.Interval = 0.2D;
+            chartArea2.AxisY.IsMarginVisible = false;
+            chartArea2.AxisY.MajorGrid.Enabled = false;
+            chartArea2.AxisY.Maximum = 1D;
+            chartArea2.AxisY.Minimum = 0D;
+            chartArea2.AxisY.ScaleView.SizeType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea2.AxisY.Title = "Probability of Getting Cherry";
+            chartArea2.Name = "ProbabilityChart";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ProbabilityChart";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Square;
+            series3.Name = "P(c | D)";
+            series4.ChartArea = "ProbabilityChart";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.IsVisibleInLegend = false;
+            series4.Legend = "Legend1";
+            series4.Name = "Series2";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(448, 268);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Select File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.fileSelectButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 310);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "No file selected.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 277);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "h1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 274);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(36, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(358, 305);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Plot";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.plotButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "h2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(127, 274);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(36, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(192, 277);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "h3";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(217, 274);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(36, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(282, 277);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "h4";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(307, 274);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(36, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(372, 277);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "h5";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(397, 274);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(36, 20);
+            this.textBox5.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(12, 341);
+            this.label7.MaximumSize = new System.Drawing.Size(350, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(449, 363);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Candy Probability";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Bayes-Candy/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private double h1 = 0.1, h2 = 0.2, h3 = 0.4, h4 = 0.2, h5 = 0.1; // default values for the hypotheses
+
+        private bool properHSum = true; // check for whether the hypotheses probabilities add up to 1
+        private bool properHVal = true; // check for whether the hypotheses textboxes contain numeric values
+        private bool fileSelected = false; // check for whether a file is selected
+        private bool parseSuccessful = true; // check for whether the file was successfully parsed
+        private bool loaded = false; // check whether the graph has been loaded yet
+
+        private char[] data; // the data from the file
+        private string[] filepath; // the array containing parts of the full filepath (separated by the '\' delimiter)
+        private string[] lines; // the set of all strings in the input text file
+        private string path; // the full filepath string
+
+        // the series of points corresponding to the hypothesis of the same number
+        private System.Windows.Forms.DataVisualization.Charting.Series series1, series2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fileSelectButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "txt files (*.txt)|*.txt";
+            file.RestoreDirectory = true;
+            
+            // get the filepath, read the file's contents, and update the graph
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                path = file.FileName;
+                filepath = file.FileName.Split(new char[] {'\\'});
+                this.label1.Text = filepath[filepath.Length - 1];
+                fileSelected = true;
+            }
+        }
+
+        private void plotButton_Click(object sender, EventArgs e)
+        {
+            properHSum = true;
+            properHVal = true;
+            parseSuccessful = true;
+            label7.Text = "";
+
+            if (!fileSelected)
+                label7.Text = "File is not selected";
+            else
+            {
+                // check that h values are Doubles
+                try
+                {
+                    h1 = Convert.ToDouble(textBox1.Text.ToString());
+                    h2 = Convert.ToDouble(textBox2.Text.ToString());
+                    h3 = Convert.ToDouble(textBox3.Text.ToString());
+                    h4 = Convert.ToDouble(textBox4.Text.ToString());
+                    h5 = Convert.ToDouble(textBox5.Text.ToString());
+                }
+                catch (FormatException)
+                {
+                    label7.Text = "h values must be real numbers between 0 and 1, inclusive";
+                    properHVal = false;
+                }
+                catch (OverflowException)
+                {
+                    label7.Text = "h values must be within the range of a Double";
+                    properHVal = false;
+                }
+
+                if (properHVal)
+                {
+                    // check that h values are positive
+                    if (h1 < 0 || h2 < 0 || h3 < 0 || h4 < 0 || h5 < 0)
+                    {
+                        label7.Text = "h values must be real numbers between 0 and 1, inclusive";
+                        properHVal = false;
+                    }
+                    // check that h values sum to 1 (this procedure is different for floating-point values)
+                    else if (Math.Abs((h1 + h2 + h3 + h4 + h5) - 1) >= 0.000000001)
+                    {
+                        label7.Text = "The sum of h values must be 1";
+                        properHSum = false;
+                    }
+                }
+            }
+
+            if (properHSum && properHVal && fileSelected)
+            {
+                // read all lines in the file and store them into variable lines
+                lines = System.IO.File.ReadAllLines(path);
+
+                // initialize the character array for the flavor data
+                data = new char[lines.Length];
+
+                // variable storing the current newline-delimited character in the file
+                char check;
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    // if the current line in the file has more than one character, then parse was unsuccessful
+                    if (lines[i].Length != 1)
+                    {
+                        label7.Text = "Each line in the file must contain one character (c or l)";
+                        parseSuccessful = false;
+                        i = lines.Length;
+                    }
+                    else
+                    {
+                        check = lines[i].ToLower()[0];
+
+                        // if the current newline-delimited character is not 'c' or 'l', then parse was unsuccessful
+                        if (check != 'c' && check != 'l')
+                        {
+                            label7.Text = "File can only contain newline-delimited characters (c or l)";
+                            parseSuccessful = false;
+                            i = lines.Length;
+                        }
+                        else
+                            data[i] = check;
+                    }
+                }
+                
+                // fill the graph with the data points for each hypothesis if file parsing was successful
+                if(parseSuccessful)
+                    fillGraph();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = h1.ToString();
+            textBox2.Text = h2.ToString();
+            textBox3.Text = h3.ToString();
+            textBox4.Text = h4.ToString();
+            textBox5.Text = h5.ToString();
+
+            series1 = chart1.Series["P(c | D)"];
+
+            // empty series that exists in order to properly plot the hypotheses data points
+            series2 = chart1.Series["Series2"];
+        }
+
+        private void fillGraph()
+        {
+            series1.Points.Clear();
+
+            if (!loaded)
+            {
+                series2.Points.AddXY(0, 0);
+                series2.Points.AddXY(1, 0);
+                series2.Points.FindByValue(0).IsEmpty = true;
+                loaded = true;
+            }
+
+            int cherryCount = 0;
+            int limeCount = 0;
+
+            double alpha;
+            double P_h1, P_h2, P_h3, P_h4, P_h5;
+            double x_value, y_value;
+
+            P_h1 = Math.Pow(1, 0) * Math.Pow(0, limeCount) * h1;
+            P_h2 = Math.Pow(1, 0) * Math.Pow(0, limeCount) * h2;
+            P_h3 = Math.Pow(1, 0) * Math.Pow(0, limeCount) * h3;
+            P_h4 = Math.Pow(1, 0) * Math.Pow(0, limeCount) * h4;
+            P_h5 = Math.Pow(1, 0) * Math.Pow(0, limeCount) * h5;
+
+            alpha = 1 / (P_h1 + P_h2 + P_h3 + P_h4 + P_h5);
+
+            x_value = 0;
+            y_value = 1 * P_h1 + 0.75 * P_h2 + 0.5 * P_h3 + 0.25 * P_h4 + 0 * P_h5;
+
+            // plot the initial point
+            series1.Points.AddXY(x_value, y_value);
+            
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] == 'c')
+                    cherryCount++;
+                else
+                    limeCount++;
+
+                // Formula: P(hi | D) = alpha * P(D | hi) * P(hi)
+                // where alpha = 1 / ( Sum of P(hi | D) over i = 1 to 5 )
+
+                P_h1 = Math.Pow(1, cherryCount) * Math.Pow(0, limeCount) * h1;
+                P_h2 = Math.Pow(0.75, cherryCount) * Math.Pow(0.25, limeCount) * h2;
+                P_h3 = Math.Pow(0.5, cherryCount) * Math.Pow(0.5, limeCount) * h3;
+                P_h4 = Math.Pow(0.25, cherryCount) * Math.Pow(0.75, limeCount) * h4;
+                P_h5 = Math.Pow(0, cherryCount) * Math.Pow(1, limeCount) * h5;
+
+                alpha = 1 / (P_h1 + P_h2 + P_h3 + P_h4 + P_h5);
+
+                P_h1 *= alpha;
+                P_h2 *= alpha;
+                P_h3 *= alpha;
+                P_h4 *= alpha;
+                P_h5 *= alpha;
+
+                x_value = i + 1;
+                y_value = 1 * P_h1 + 0.75 * P_h2 + 0.5 * P_h3 + 0.25 * P_h4 + 0 * P_h5;
+
+                series1.Points.AddXY(x_value, y_value);
+            }
+        }
+    }
+}
